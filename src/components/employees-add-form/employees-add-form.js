31d9b135b45283592e3a76e7c2f38bda9df45aa8
Daniel <Danil.danek@gmail.com>
2023-03-22 12:59:46 +0100

    React Controll input elements

--- src/components/employees-add-form/employees-add-form.js
+++ src/components/employees-add-form/employees-add-form.js
@@ -1,23 +1,50 @@
+import { Component } from 'react'
 import './employees-add-form.css'
 
-const EmployeeAddForm = () => {
-  return (
-    <div className="app-add-form">
-        <h3>Add new employee</h3>
-        <form
-            className="add-form d-flex">
-            <input type="text"
-                className="form-control new-post-label"
-                placeholder="What is his name?" />
-            <input type="number"
-                className="form-control new-post-label"
-                placeholder="Salary in $?" />
+class EmployeeAddForm extends Component{
+  constructor(props) {
+    super(props);
 
-            <button type="submit"
-                    className="btn btn-outline-light">Add</button>
-        </form>
-    </div>
-  )
+    this.state = {
+      name: '',
+      salary: '',
+    }
+  }
+
+  onValueChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  render() {
+    const {name, salary} = this.state;
+
+    return (
+      <div className="app-add-form">
+          <h3>Add new employee</h3>
+          <form
+          /* При связывании state в value мы делаем элемент контроллируемым что улучшает производительность */
+              className="add-form d-flex">
+              <input type="text"
+                  className="form-control new-post-label"
+                  placeholder="What is his name?" 
+                  name='name'
+                  value={name}
+                  onChange={this.onValueChange}/>
+              <input type="number"
+                  className="form-control new-post-label"
+                  placeholder="Salary in $?"
+                  name='salary' 
+                  value={salary}
+                  onChange={this.onValueChange}/>
+
+              <button type="submit"
+                  className="btn btn-outline-light">Add</button>
+          </form>
+      </div>
+    )
+  }
 }
 
 export default EmployeeAddForm
